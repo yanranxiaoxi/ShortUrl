075ce75690e0818caa462733c85f953b8fa41c7b
萌小汐 <CollageTomato@users.noreply.github.com>
2016-10-16 11:40:53 +0800

    更新一个间接性阻止跳转的错误

--- config.php
+++ config.php
@@ -10,8 +10,8 @@ define('SITE_TITLE', 'ShortUrl'); //网页标题
 define('ADMIN_USERNAME', 'admin'); //管理员用户名
 define('ADMIN_PASSWORD', 'password'); //管理员密码
 define('URL_PROTOCOLS', 'http|https|ftp|ftps|mailto|news|mms|rtmp|rtmpt|e2dk'); //允许的协议
-define('SHORTURL_VERSION', '1.0.1');//版本号，请勿修改
-define('SHORTURL_NUMERICVERSION', '101');//版本号比对形式，请勿修改
+define('SHORTURL_VERSION', '1.0.2');//版本号，请勿修改
+define('SHORTURL_NUMERICVERSION', '102');//版本号比对形式，请勿修改
 define('INDIRECTLYGO','0'); //开启跳转等待请将值更改为“1”，反之为“0”
 define('GOTIME','10');	//跳转等待的时间
 error_reporting(E_ALL);
