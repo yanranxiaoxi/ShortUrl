b336880b2964d552096b05c7827b347ee2cde581
萌小汐 <CollageTomato@users.noreply.github.com>
2016-10-16 11:41:32 +0800

    更新一个间接性阻止跳转的错误

--- html/index.php
+++ html/index.php
@@ -108,7 +108,7 @@ ini_set('display_errors', 0);
 	</form>
 </section>
 <footer>
-	<p>Copyright © <?php echo date('Y'); ?> <a href="http://moe-girl.club">萌娘基金会</a> , All Rights Reserved.</p>
+	<p>Copyright © <?php echo date('Y'); ?> <a href="http://team.moe-girl.co">萌娘基金会</a> , All Rights Reserved.</p>
 </footer>
 </body>
 </html>
